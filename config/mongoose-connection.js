5a54a930eed9a6a821d177e10f8bdaa74c19341f
sohelhussain <sohelhussaing@gmail.com>
2024-07-26 11:15:11 +0530

    disturb the main branhc

--- config/mongoose-connection.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const config = require('config');
-mongoose
-.connect(`${config.get("MONGODB_URI")}/wedingram`)
-.then(()=>{
-    console.log("database connection established");
-})
-.catch(err => console.error(err));
