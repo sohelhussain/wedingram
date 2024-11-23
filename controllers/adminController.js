c209a1b79c3d7820c32578b01627c07f8d99a3ef
sohelhussain <sohelhussaing@gmail.com>
2024-11-23 11:08:03 +0530

    feat: CREATIN OF ROUTE

---
+++ controllers/adminController.js
@@ -0,0 +1,11 @@
+
+
+// const {cyberModel} = require('../models/cyberModel');
+
+module.exports.cyberActive = async (req, res) => {
+    res.send('this is cyber Active page');
+}
+
+module.exports.cyberInActive = async (req, res) => {
+    res.send('this is cyber active to inactive');
+}
--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,10 +2,14 @@ const express = require("express");
 const router = express.Router();
 const { adminPageController } = require("../controllers/creatingAdmin");
 // const { cyberAuthPage } = require("../middlewares/adminCyber-middleware");
+const {cyberActive, cyberInActive} = require('../controllers/adminController');
 
 // cyberAuthPage(["cyber"]),
 
 router.get("/", adminPageController);
+
+router.get("/active/:cyberId", cyberActive)
+router.get("/inactive/:cyberId", cyberInActive)
 
 
 module.exports = router;
